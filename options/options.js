a3c061b7df7bf86c9f2b5c018e94c8457c8f4911
Crowdbotics <engineering@crowdbotics.com>
2022-11-04 13:23:31 +0000

    Update options

--- options/options.js
+++ options/options.js
@@ -1,12 +1,14 @@
-import { StyleSheet } from "react-native";
+import { StyleSheet } from "react-native"
 
 export const globalOptions = {
   name: "figmareg27_dev_42771",
   url: "https://figmareg27_dev_42771.botics.co",
   api: "https://figmareg27_dev_42771.botics.co/api/v1"
 }
+
 export const modulesOptions = {
   "@modules/app-menu": {
-    "copy": "Routes available!"
-  }
+    copy: "Routes available!"
+  },
+  undefined: { title: "App Menu 1", copy: "Routes available!", styles: style }
 }
