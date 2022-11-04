6b01ea661a85c49002225dbe2cf6cd0fc101b481
Crowdbotics <engineering@crowdbotics.com>
2022-11-04 13:33:44 +0000

    Update options

--- options/options.js
+++ options/options.js
@@ -1,4 +1,18 @@
 import { StyleSheet } from "react-native"
+const styles = StyleSheet.create({
+  hr: {
+    marginTop: 20,
+    marginBottom: 20,
+    borderBottomColor: "black",
+    borderBottomWidth: 1.5
+  },
+  container: { flex: 1, height: 100, padding: 13 },
+  text: { color: "black", fontSize: 20 },
+  buttonPressed: { backgroundColor: "aquamarine" },
+  buttonNotPressed: { backgroundColor: "blue" },
+  button: { borderRadius: 4, padding: 15, marginTop: 10 },
+  buttonText: { color: "white", textAlign: "center", fontSize: 16 }
+})
 
 export const globalOptions = {
   name: "figmareg27_dev_42771",
@@ -8,7 +22,10 @@ export const globalOptions = {
 
 export const modulesOptions = {
   "@modules/app-menu": {
-    copy: "Routes available!"
+    title: "App Menu",
+    copy: "Routes available!",
+    styles: styles
   },
+
   undefined: { title: "App Menu 1", copy: "Routes available!", styles: style }
 }
